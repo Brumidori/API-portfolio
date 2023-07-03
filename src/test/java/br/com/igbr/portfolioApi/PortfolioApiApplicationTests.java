57c7d490ffd0c33643f40c1a7907e385f6d7f107
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-03 15:44:39 -0300

    initial commit

---
+++ src/test/java/br/com/igbr/portfolioApi/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igbr.portfolioApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
